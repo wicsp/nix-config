d99d56092acd4e7008af488aa4e4d56e8da56e22
wicsp <wicspa@gmail.com>
2024-11-15 02:50:05 +0800

    refine

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,23 @@
       ./orbstack.nix
     ];
   nix.settings.experimental-features=[ "nix-command" "flakes"];
+  # do not need to keep too much generations
+  boot.loader.systemd-boot.configurationLimit = 10;
+  # boot.loader.grub.configurationLimit = 10;
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimise storage
+  # you can also optimise the store manually via:
+  #    nix-store --optimise
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+
   environment.systemPackages = with pkgs; [
 git
 vim
