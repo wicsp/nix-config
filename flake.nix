de0a9cc8cdd9de2a69cf745db6ad8dcf6bfd9f42
wicsp <wicspa@gmail.com>
2024-12-09 17:56:22 +0800

    update to darwin version

--- flake.nix
+++ flake.nix
@@ -1,99 +1,39 @@
 {
-  description = "A simple NixOS flake";
+  description = "Example nix-darwin system flake";
 
-  inputs = {
-    # NixOS 官方软件源，这里使用 nixos-23.11 分支
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+  nixConfig = {
+    substituters = [
+      # Query the mirror of USTC first, and then the official cache.
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
   };
 
-  outputs = inputs@{self, nixpkgs, nixpkgs-stable, home-manager, ... 
-  }: let
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, darwin, nixpkgs, ... }:
+  let
     username = "wicsp";
-    useremail = "wicspa@gmail.com";
-
-    x64_system = "x86_64-linux";
-    aarch64_system = "aarch64-linux";
-    aarch64_darwin = "aarch64-darwin";
-    allSystems = [ x64_system aarch64_system aarch64_darwin ];
-
-    nixosSystem = import ./lib/nixosSystem.nix;
-    macosSystem = import ./lib/macosSystem.nix;
-
-  in {
-
-    nixosConfigurations =  let
-      # 星野 アイ, Hoshino Ai
-      nixorb_modules = {
-        nixos-modules = [
-          ./hosts/idols/ai
-        ];
-        home-module = import ./home/linux/desktop-i3.nix;
+    hostname = "macsp";
+    specialArgs =
+      inputs
+      // {
+        inherit username hostname;
       };
-      specialArgs =
-        {
-          inherit username useremail;
-          # use unstable branch for some packages to get the latest updates
-          pkgs-unstable = import nixpkgs-unstable {
-            system = x64_system; # refer the `system` parameter form outer scope recursively
-            # To use chrome, we need to allow the installation of non-free software
-            config.allowUnfree = true;
-          };
-        }
-        // inputs;
-      base_args = {
-        inherit home-manager nixos-generators system specialArgs;
-      };
-      stable_args = base_args // {inherit nixpkgs;};
-      unstable_args = base_args // {nixpkgs = nixpkgs-unstable;};
-    in
-    {
-      # 这里的 my-nixos 替换成你的主机名称
-      nixorb = nixpkgs.lib.nixosSystem rec{
-        system = "aarch64-linux";
-        # 核心参数是这个，将非默认的 nixpkgs 数据源传到其他 modules 中
-        specialArgs = {
-          # 注意每次 import 都会生成一个新的 nixpkgs 实例
-          # 这里我们直接在 flake.nix 中创建实例， 再传递到其他子 modules 中使用
-          # 这样能有效重用 nixpkgs 实例，避免 nixpkgs 实例泛滥。
-          pkgs-stable = import nixpkgs-stable {
-            # 这里递归引用了外部的 system 属性
-            inherit system;
-            # 为了拉取 chrome 等软件包，
-            # 这里我们需要允许安装非自由软件
-            config.allowUnfree = true;
-          };
-        };
-        modules = [
-          ./configuration.nix
-
-          # 将 home-manager 配置为 nixos 的一个 module
-          # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            # 这里的 ryan 也得替换成你的用户名
-            # 这里的 import 函数在前面 Nix 语法中介绍过了，不再赘述
-            home-manager.users.wicsp = import ./home.nix;
-
-            # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
-            # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
-            # home-manager.extraSpecialArgs = inputs;
-          }
-        ];
-      };
+  in
+  {
+    system.configurationRevision = self.rev or self.dirtyRev or null;
+    darwinConfigurations."macsp" = darwin.lib.darwinSystem {
+      inherit specialArgs;
+      modules = [ 
+        ./modules/nix-core.nix
+        ./modules/system.nix
+        ./modules/apps.nix
+      ];
     };
   };
 }
-
