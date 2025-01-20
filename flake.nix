16bde5395da3d6e20abaa227075287edd8a8fd5d
wicsp <wicspa@gmail.com>
2025-01-20 16:55:03 +0800

    update nix files

---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  description = "A simple NixOS flake";
+
+  nixConfig = {
+    substituters = [
+      # Query the mirror of USTC first, and then the official cache.
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
+  };
+
+  inputs = {
+    # NixOS 官方软件源，这里使用 nixos-24.11 分支
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+    # home-manager, used for managing user configuration
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager-darwin.url = "github:nix-community/home-manager/release-24.11";
+    home-manager-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-darwin, darwin, home-manager, home-manager-darwin, vscode-server, ... } @ inputs: let 
+  username = "wicsp";
+  usermail = "wicspa@gmail.com";
+  specialArgs = inputs // {inherit username usermail;};
+  in{
+    nixosConfigurations = {
+      nixpara= nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        system = "aarch64-linux";
+        modules = [
+          ./hosts/nixpara
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} = import ./hosts/nixpara/home.nix;
+          }
+          vscode-server.nixosModules.default
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
+        ];
+      };
+
+      nixos=nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nixos
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} = import ./hosts/nixos/home.nix;
+          }
+          vscode-server.nixosModules.default
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
+        ];
+      };
+    };
+
+    darwinConfigurations = {
+      macsp =  darwin.lib.darwinSystem {
+        inherit specialArgs;
+        system = "aarch64-darwin";
+        modules = [
+          ./hosts/macsp
+          home-manager-darwin.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs =  specialArgs;
+            home-manager.users.${username} = import ./hosts/macsp/home.nix;
+          }
+        ];
+      };
+    };
+    system.configurationRevision = self.rev or self.dirtyRev or null;
+   };
+}
