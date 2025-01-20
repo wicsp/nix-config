114f11ab59bad34ef4b3a8fe72f52b888583423e
wicsp <wicspa@gmail.com>
2025-01-20 17:17:33 +0800

    iii

--- modules/core.nix
+++ modules/core.nix
@@ -8,7 +8,6 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
-    isNormalUser = true;
     description = username;
     home = "/home/${username}";  # 指定家目录路径
     createHome = true;  # 自动创建家目录
