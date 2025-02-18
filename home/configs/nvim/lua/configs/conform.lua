6c0c0ec6b80dbd73a41b1e92176992f06417bec2
wicsp <wicspa@gmail.com>
2025-02-18 17:03:31 +0800

    chore

--- home/configs/nvim/lua/configs/conform.lua
+++ home/configs/nvim/lua/configs/conform.lua
@@ -3,11 +3,23 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    python = { "isort", "black" },
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
+    go = { "goimports", "gofmt" },
     -- You can customize some of the format options for the filetype (:help conform.format)
     rust = { "rustfmt", lsp_format = "fallback" },
     -- Conform will run the first available formatter
     javascript = { "prettierd", "prettier", stop_after_first = true },
+    -- Use the "*" filetype to run formatters on all filetypes.
+    ["*"] = { "codespell" },
+    -- Use the "_" filetype to run formatters on filetypes that don't
+    -- have other formatters configured.
+    ["_"] = { "trim_whitespace" },
   },
 
   format_on_save = {
