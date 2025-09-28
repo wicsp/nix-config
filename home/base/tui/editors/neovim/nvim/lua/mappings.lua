eedd43e725d58a5ce54ba9e4129b4db979fb6b2e
wicsp <wicspa@gmail.com>
2025-09-28 22:38:55 +0800

    remove jk shortcut for nvim

--- home/base/tui/editors/neovim/nvim/lua/mappings.lua
+++ home/base/tui/editors/neovim/nvim/lua/mappings.lua
@@ -5,6 +5,4 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
