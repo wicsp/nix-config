58ee45d4f572dfb240535ff06be450fbcf4cd9ae
wicsp <wicspa@gmail.com>
2025-10-23 16:57:11 +0800

    try to fix anyrun

--- home/linux/gui/hyprland/default.nix
+++ home/linux/gui/hyprland/default.nix
@@ -9,7 +9,7 @@ with lib; let
   cfg = config.modules.desktop.hyprland;
 in {
   imports = [
-    anyrun.homeManagerModules.default
+    # anyrun.homeManagerModules.default  # commented out due to conflict with values/anyrun.nix
     ./options
   ];
 
