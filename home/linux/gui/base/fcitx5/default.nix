de9b7e4f312f3712f1b11e834563bb3fed88af3a
wicsp <wicspa@gmail.com>
2025-10-23 19:37:45 +0800

    try fix fcitx5 2

--- home/linux/gui/base/fcitx5/default.nix
+++ home/linux/gui/base/fcitx5/default.nix
@@ -19,14 +19,14 @@
     enable = true;
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs; [
-      # for flypy chinese input method
-      fcitx5-rime
-      # needed enable rime using configtool after installed
-      fcitx5-configtool
-      # fcitx5-chinese-addons # we use rime instead
-      # fcitx5-mozc    # japanese input method
-      fcitx5-gtk # gtk im module
-    ];
+    # fcitx5.addons = with pkgs; [
+    #   # for flypy chinese input method
+    #   fcitx5-rime
+    #   # needed enable rime using configtool after installed
+    #   fcitx5-configtool
+    #   # fcitx5-chinese-addons # we use rime instead
+    #   # fcitx5-mozc    # japanese input method
+    #   fcitx5-gtk # gtk im module
+    # ];
   };
 }
