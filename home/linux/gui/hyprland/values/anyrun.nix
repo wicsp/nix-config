4b1892c582437a405b35e2d1eec5417ea8790b86
wicsp <wicspa@gmail.com>
2025-10-23 16:04:28 +0800

    disable anyrun temply

--- home/linux/gui/hyprland/values/anyrun.nix
+++ home/linux/gui/hyprland/values/anyrun.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   programs.anyrun = {
-    enable = true;
+    enable = false; # TODO: wait to repair
     config = {
       plugins = with anyrun.packages.${pkgs.system}; [
         applications
