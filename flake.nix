1deaef8dcceb51068401be0d9654b3a05183efe2
wicsp <wicspa@gmail.com>
2025-10-24 21:54:23 +0800

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
 {
-  description = "A simple NixOS flake";
+  description = "WICSP's nix configuration for both NixOS and macOS";
+  # this is yoink from ryan4yin XD
+
+  ##################################################################################################################
+  #
+  # Want to know Nix in details? Looking for a beginner-friendly tutorial?
+  # Check out https://github.com/ryan4yin/nixos-and-flakes-book !
+  #
+  ##################################################################################################################
 
   outputs = inputs: import ./outputs inputs;
 
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  # for more information, see:
+  #     https://nixos-and-flakes.thiscute.world/nix-store/add-binary-cache-servers
   nixConfig = {
     # substituers will be appended to the default substituters when fetching packages
     extra-substituters = [
-      "https://anyrun.cachix.org"
-      "https://nix-gaming.cachix.org"
-      "https://nixpkgs-wayland.cachix.org"
+      # "https://anyrun.cachix.org"
+      # "https://nix-gaming.cachix.org"
+      # "https://nixpkgs-wayland.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      # "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      # "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
 
     substituters = [
@@ -26,12 +37,21 @@
     trusted-users = ["root" "wicsp"];
   };
 
+  # This is the standard format for flake.nix. `inputs` are the dependencies of the flake,
+  # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
   inputs = {
+    # There are many ways to reference flake inputs. The most widely used is github:owner/name/reference,
+    # which represents the GitHub repository URL + branch/commit-id/tag.
+
     # Official NixOS package source, using nixos's unstable branch by default
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    nixpkgs-ollama.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # nixpkgs-patched.url = "github:ryan4yin/nixpkgs/nixos-unstable-patched";  # custom nixpkgs with some patches，i dont have one yet
 
     # for macos
     # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-25.05-darwin";
@@ -40,11 +60,12 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
-    darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
-    };
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    # darwin = {
+    #   url = "github:LnL7/nix-darwin";
+    #   inputs.nixpkgs.follows = "nixpkgs-darwin";
+    # };
+    # nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # home-manager, used for managing user configuration
     home-manager = {
@@ -57,42 +78,56 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # https://github.com/catppuccin/nix
+    catppuccin = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    preservation = {
+      url = "github:nix-community/preservation";
     };
 
     impermanence.url = "github:nix-community/impermanence";
 
     # community wayland nixpkgs
     # nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+
     # anyrun - a wayland launcher
     anyrun = {
-      url = "github:Kirottu/anyrun";
+      url = "github:/anyrun-org/anyrun/v25.9.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # generate iso/qcow2/docker/... image from nixos configuration
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # TODO lock agenix
     agenix.url = "github:yaxitech/ragenix";
 
-    nix-gaming.url = "github:fufexan/nix-gaming";
-
     disko = {
-      url = "github:nix-community/disko/v1.9.0";
+      url = "github:nix-community/disko/v1.11.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # add git hooks to format nix code before commit
     pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+      url = "github:cachix/pgit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nuenv.url = "github:DeterminateSystems/nuenv";
+    nuenv = {
+      url = "github:DeterminateSystems/nuenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     haumea = {
       url = "github:nix-community/haumea/v0.2.2";
@@ -108,7 +143,35 @@
       url = "github:ghostty-org/ghostty";
     };
 
+    blender-bin = {
+      url = "github:edolstra/nix-warez?dir=blender";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # i dont need a nixos-apple-silicon repo yet, i am using nix-darwin for my macsp
+    # nixos-apple-silicon = {
+    #   # 2025-10-07 asahi-6.16.8-1
+    #   url = "github:nix-community/nixos-apple-silicon/24ab28e47b586f741910b3a2f0428f3523a0fff3";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    niri.url = "github:sodiboo/niri-flake";
+
+    # -------------- Gaming ---------------------
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     ########################  Some non-flake repositories  #########################################
+    nu_scripts = {
+      url = "github:nushell/nu_scripts";
+      flake = false;
+    };
 
     ########################  My own repositories  #########################################
 
@@ -119,16 +182,26 @@
       flake = false;
     };
 
+    # i dont need a asahi-firmware repo yet, i am using nix-darwin for my macsp
+    # my-asahi-firmware = {
+    #   url = "git+ssh://git@github.com/ryan4yin/asahi-firmware.git?shallow=1";
+    #   flake = false;
+    # };
+
     # my wallpapers
     wallpapers = {
-      url = "github:ryan4yin/wallpapers";
+      url = "github:wicsp/wallpapers";
       flake = false;
     };
 
-    # nur-ryan4yin.url = "github:ryan4yin/nur-packages";
-    # nur-ataraxiasjel.url = "github:AtaraxiaSjel/nur";
+    # nur-ryan4yin = {
+    #   url = "github:ryan4yin/nur-packages";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    vscode-server = {
+      url = "github:nix-community/nixos-vscode-server";
+    };
   };
 
   # outputs = { self, nixpkgs, nixpkgs-darwin, darwin, home-manager, home-manager-darwin, vscode-server, ... } @ inputs: let
---
+++ home/linux/gui/niri/default.nix
@@ -0,0 +1,61 @@
+{
+  pkgs,
+  config,
+  lib,
+  niri,
+  ...
+} @ args: let
+  cfg = config.modules.desktop.niri;
+in {
+  options.modules.desktop.niri = {
+    enable = lib.mkEnableOption "niri compositor";
+    settings = lib.mkOption {
+      type = with lib.types; let
+        valueType =
+          nullOr (oneOf [
+            bool
+            int
+            float
+            str
+            path
+            (attrsOf valueType)
+            (listOf valueType)
+          ])
+          // {
+            description = "niri configuration value";
+          };
+      in
+        valueType;
+      default = {};
+    };
+  };
+
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        home.packages = with pkgs; [
+          # Niri v25.08 will create X11 sockets on disk, export $DISPLAY, and spawn `xwayland-satellite` on-demand when an X11 client connects
+          xwayland-satellite
+        ];
+
+        programs.niri.config = cfg.settings;
+
+        # NOTE: this executable is used by greetd to start a wayland session when system boot up
+        # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
+        home.file.".wayland-session" = {
+          source = pkgs.writeScript "init-session" ''
+            # trying to stop a previous niri session
+            systemctl --user is-active niri.service && systemctl --user stop niri.service
+            # and then we start a new one
+            /run/current-system/sw/bin/niri-session
+          '';
+          executable = true;
+        };
+      }
+      (import ./settings.nix niri)
+      (import ./keybindings.nix niri)
+      (import ./spawn-at-startup.nix niri)
+      (import ./windowrules.nix niri)
+    ]
+  );
+}
