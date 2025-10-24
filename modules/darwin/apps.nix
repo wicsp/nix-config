1afd18cd8eef73d15adc6fb44c93c0ec97985d94
wicsp <wicspa@gmail.com>
2025-10-24 23:49:59 +0800

    go through modules/darwin

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -33,17 +33,24 @@ let
     # HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
     # HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
 
-    # nju mirror
-    HOMEBREW_API_DOMAIN = "https://mirror.nju.edu.cn/homebrew-bottles/api";
-    HOMEBREW_BOTTLE_DOMAIN = "https://mirror.nju.edu.cn/homebrew-bottles";
-    HOMEBREW_BREW_GIT_REMOTE = "https://mirror.nju.edu.cn/git/homebrew/brew.git";
-    HOMEBREW_CORE_GIT_REMOTE = "https://mirror.nju.edu.cn/git/homebrew/homebrew-core.git";
+    # bfsu mirror
+    HOMEBREW_API_DOMAIN = "https://mirrors.bfsu.edu.cn/homebrew-bottles/api";
+    HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.bfsu.edu.cn/homebrew-bottles";
+    HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.bfsu.edu.cn/git/homebrew/brew.git";
+    HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.bfsu.edu.cn/git/homebrew/homebrew-core.git";
     HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
+
+    # nju mirror
+    # HOMEBREW_API_DOMAIN = "https://mirror.nju.edu.cn/homebrew-bottles/api";
+    # HOMEBREW_BOTTLE_DOMAIN = "https://mirror.nju.edu.cn/homebrew-bottles";
+    # HOMEBREW_BREW_GIT_REMOTE = "https://mirror.nju.edu.cn/git/homebrew/brew.git";
+    # HOMEBREW_CORE_GIT_REMOTE = "https://mirror.nju.edu.cn/git/homebrew/homebrew-core.git";
+    # HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
   };
 
   local_proxy_env = {
-    HTTP_PROXY = "http://127.0.0.1:7890";
-    HTTPS_PROXY = "http://127.0.0.1:7890";
+    # HTTP_PROXY = "http://127.0.0.1:7890";
+    # HTTPS_PROXY = "http://127.0.0.1:7890";
   };
 
   homebrew_env_script =
@@ -82,7 +89,11 @@ in {
     ${homebrew_env_script}
   '';
 
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  # this is required if you want to use darwin's default shell - zsh
+  programs.zsh.enable = true;
   environment.shells = [
+    pkgs.zsh
     pkgs-unstable.nushell # my custom shell
   ];
 
@@ -124,7 +135,6 @@ in {
       "wget" # download tool
       "curl" # no not install curl via nixpkgs, it's not working well on macOS!
       "aria2" # download tool
-      # "httpie" # http client
       "wireguard-tools" # wireguard
 
       # a markdown editor
@@ -136,7 +146,7 @@ in {
       #  https://github.com/tailscale/tailscale/wiki/Tailscaled-on-macOS#run-the-tailscaled-daemon
       # 1. `sudo tailscaled install-system-daemon`
       # 2. `tailscale up --accept-routes`
-      "tailscale" # tailscale
+      # "tailscale" # tailscale
 
       # https://github.com/rgcr/m-cli
       "m-cli" #  Swiss Army Knife for macOS
@@ -150,48 +160,60 @@ in {
       "git-trim"
       "terraform"
       "terraformer"
-
-      "opencv"
-      "eigen"
     ];
 
     # `brew install --cask`
     casks = [
       "flashspace"
 
-      "squirrel" # input method for Chinese, rime-squirrel
+      "squirrel-app" # input method for Chinese, rime-squirrel
       "firefox"
       "google-chrome"
+
       "visual-studio-code"
       "zed" # zed editor
+      "cursor" # cursor ai editor
+
       "aerospace" # an i3-like tiling window manager for macOS
       "ghostty" # terminal emulator
 
       # https://joplinapp.org/help/
       # "joplin" # note taking app
 
+      "tailscale-app" # tailscale macos app (with gui)
+      # "netbirdio/tap/netbird-ui" # netbird gui app
+
+      # AI
+      # "lm-studio"
+
       # IM & audio & remote desktop & meeting
       "telegram"
+      # "qq"
+      # "qqmusic"
       # "discord" # update too frequently, use the web version instead
       # "microsoft-remote-desktop"
       # "moonlight" # remote desktop client
       # "rustdesk"
+      # "zoom" # meeting
 
       # Misc
       # "shadowsocksx-ng" # proxy tool
-      "iina" # video player
+      # "iina" # video player
       "raycast" # (HotKey: alt/option + space)search, calculate and run scripts(with many plugins)
-      "stats" # beautiful system status monitor in menu bar
+      # "stats" # beautiful system status monitor in menu bar
+      # "jordanbaird-ice" # Powerful menu bar manager for macOS
+
       # "reaper"  # audio editor
       # "sonic-pi" # music programming
       # "tencent-lemon" # macOS cleaner
       "neteasemusic" # music
       # "blender@lts" # 3D creation suite
-      # "ibkr"
+      # "clash-verge-rev" # the same as mihomo-party
 
       # Development
       "mitmproxy" # HTTP/HTTPS traffic inspector
       "insomnia" # REST client
+      # "wireshark-app" # network analyzer
       # "jdk-mission-control" # Java Mission Control
       # "google-cloud-sdk" # Google Cloud SDK
       # "miniforge" # Miniconda's community-driven distribution
