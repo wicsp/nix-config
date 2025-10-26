758882f8d2fe56e3274829a11e3c5712554e3ca9
wicsp <wicspa@gmail.com>
2025-10-27 01:35:51 +0800

    go through output/src/goudan

--- hardening/nixpaks/default.nix
+++ hardening/nixpaks/default.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  pkgs-patched,
+  # pkgs-patched,
   nixpak,
   ...
 }:
@@ -22,7 +22,7 @@ in
   nixpkgs.overlays = [
     (_: super: {
       nixpaks = {
-        qq = wrapper pkgs-patched ./qq.nix;
+        # qq = wrapper pkgs-patched ./qq.nix;
         wechat = wrapper super ./wechat.nix;
         telegram-desktop = wrapper super ./telegram-desktop.nix;
         firefox = wrapper super ./firefox.nix;
