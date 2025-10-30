aa7c0f2698fbab2c30a0780f2cfb38dbf045f62b
wicsp <wicspa@gmail.com>
2025-10-30 19:42:26 +0800

    update nomad ip

--- hosts/goudan/nomad.nix
+++ hosts/goudan/nomad.nix
@@ -3,9 +3,9 @@
     bind_addr = "0.0.0.0";
     advertise = {
       # Defaults to the first private IP address.
-      http = "goudan:4646";
-      rpc = "goudan:4647";
-      serf = "goudan:4648"; # non-default ports may be specified
+      http = "100.100.1.3:4646";
+      rpc = "100.100.1.3:4647";
+      serf = "100.100.1.3:4648"; # non-default ports may be specified
     };
 
     server = {
--- hosts/nexus/nomad.nix
+++ hosts/nexus/nomad.nix
@@ -3,11 +3,11 @@
     bind_addr = "0.0.0.0";
     advertise = {
       # Defaults to the first private IP address.
-      http = "nexus:4646";
-      rpc = "nexus:4647";
-      serf = "nexus:4648"; # non-default ports may be specified
+      http = "100.100.10.1:4646";
+      rpc = "100.100.10.1:4647";
+      serf = "100.100.10.1:4648"; # non-default ports may be specified
     };
-    servers = [ "goudan.gate-monster.ts.net:4647" ];
+    servers = [ "100.100.1.3:4647" ];
     client = {
       enabled = true;
     };
