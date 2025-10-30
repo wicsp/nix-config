fc87b88945df063b83f16a702aebea7dc387e943
wicsp <wicspa@gmail.com>
2025-10-30 16:49:16 +0800

    add nomad

---
+++ modules/nixos/base/nomad.nix
@@ -0,0 +1,14 @@
+{
+  services.nomad = {
+    enable = true;
+    settings = {
+      server = {
+        enabled = true;
+        bootstrap_expect = 1;
+      };
+      client = {
+        enabled = true;
+      };
+    };
+  };
+}
