cf798779682684f63ab376f33197f1f4efcf864b
wicsp <wicspa@gmail.com>
2026-03-11 17:23:50 +0800

    try to fix fcitx5

--- home/linux/gui/base/fcitx5/default.nix
+++ home/linux/gui/base/fcitx5/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   xdg.configFile = {
     "fcitx5/profile" = {
@@ -14,10 +14,13 @@
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
+      qt6Packages.fcitx5-configtool # GUI for fcitx5
+      fcitx5-gtk # gtk im module
+
       # for flypy chinese input method
       fcitx5-rime
       # needed enable rime using configtool after installed
-      qt6Packages.fcitx5-configtool
+
       # fcitx5-chinese-addons # we use rime instead
       # fcitx5-mozc    # japanese input method
       # fcitx5-gtk # gtk im module
