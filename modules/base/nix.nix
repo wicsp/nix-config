82937e0e067796b438ba7763c251371a22d218a5
wicsp <wicspa@gmail.com>
2026-04-09 19:37:13 +0800

    update substituter

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -19,6 +19,8 @@
 
     # substituers that will be considered before the official ones(https://cache.nixos.org)
     substituters = [
+      "https://cache.nixos.org"
+
       # cache mirror located in China
       # status: https://mirrors.ustc.edu.cn/status/
       #"https://mirrors.ustc.edu.cn/nix-channels/store"
@@ -34,6 +36,7 @@
     ];
 
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "ryan4yin.cachix.org-1:Gbk27ZU5AYpGS9i3ssoLlwdvMIh0NxG0w8it/cv9kbU="
     ];
